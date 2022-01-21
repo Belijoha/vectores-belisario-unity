37405f9dcfcbeaffca5adef7ce23dfa18a76663e
Belijoha <johannabeli.3@gmail.com>
2022-01-20 22:33:53 -0300

    init commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float Speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Speed);
+        }
+        if (Input.GetKey(KeyCode.W))
+
+        {
+            transform.Translate(Vector3.forward * Speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+
+        {
+            transform.Translate(Vector3.back * Speed);
+        }
+
+    }
+}
